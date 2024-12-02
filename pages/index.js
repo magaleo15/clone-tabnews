a180c17e4a5768ba68ac744468966b1a32de6bbe
Leo M <leomaga.eq@gmail.com>
2024-12-02 10:39:12 -0300

    add under construction information

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1>Estou fazendo isso por vocês!! Mamãe, neném e cachorrilho!!</h1>;
+  return (
+    <h1>
+      Under Construction! This is a page in construction to impact anyone who
+      loves Science (all of them!) which will be built with help from a very
+      engaged community what will help to make information about science
+      accessible specially in Brazil. And at the same time it will help the
+      website founder (not owner since it will be onwed by the community as an
+      open source project) learning how to code.
+    </h1>
+  );
 }
 
 export default Home;
